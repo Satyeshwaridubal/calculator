d4cf3d984489ed089e4bef59b19f5502f2f46642
Satyeshwaridubal <satyeshwaridubal55@gmail.com>
2023-10-11 10:23:09 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,18 @@
+
+let display = document.getElementById("display");
+
+function appendToDisplay(value) {
+    display.textContent += value;
+}
+
+function clearDisplay() {
+    display.textContent = "";
+}
+
+function calculateResult() {
+    try {
+        display.textContent = eval(display.textContent);
+    } catch (error) {
+        display.textContent = "Error";
+    }
+}
